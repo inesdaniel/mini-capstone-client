edfe6adf81f8c51e38a20bb07502bab8825f92c1
Ines Daniel <i.danie8@gmail.com>
2018-07-10 08:58:37 -0500

    fix flash messages

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -17,21 +17,20 @@ class Client::ProductsController < ApplicationController
   end
 
   def create
-    response = Unirest.post("http://localhost:3000/api/products",
-      parameters: {
+    client_params = {
         input_name: params[:input_name],
         input_description: params[:input_description],
         input_price: params[:input_price],
         input_image_url: params[:input_image_url],
         input_stocked: params[:input_stocked]
       }
+    response = Unirest.post("http://localhost:3000/api/products",
+      parameters: client_params
       )
-    @product = response.body
     flash[:success] = "You made a new product!"
     redirect_to "/client/products"
   end
   def edit
-    flash[:success] = "You changed a product"
     product_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
     @product = response.body
@@ -49,8 +48,9 @@ class Client::ProductsController < ApplicationController
     }
     response = Unirest.patch("http://localhost:3000/api/products/#{params[:id]}",
       parameters: client_params)
-    @product = response.body
-    redirect_to "/client/products/#{@product["id"]}"
+    
+    flash[:success] = "You changed a product"
+    redirect_to "/client/products/#{params[:id]}"
   end
 
   def destroy
