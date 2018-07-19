48debc8cb68e1ea962f9eda932cf8c2593af2f24
Ines Daniel <i.danie8@gmail.com>
2018-07-18 19:32:45 -0500

    make search and sort client inputs, display images for index and show

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -1,20 +1,24 @@
 class Client::ProductsController < ApplicationController
   def index
-    user_input = params[:client_search]
+    # user_input = params[:client_search]
+    # # category = params[:]
+    # response = Unirest.get("http://localhost:3000/api/products",
+    #   parameters: {
+    #     api_search: user_input,
+    #     category: params[:category]
+    #   }
+    # )
+    # @products = response.body
+    client_params = {
+      api_search: params[:api_search],
+      sort_by: params[:client_sort_by],
+      category: params[:category]
+    }
     response = Unirest.get("http://localhost:3000/api/products",
-      parameters: {
-        api_search: user_input
-      }
+      parameters: client_params
     )
     @products = response.body
     render "index.html.erb"
-  end
-
-  def show
-    response_id = params[:id]
-    response = Unirest.get("http://localhost:3000/api/products/#{response_id}")
-    @product = response.body
-    render "show.html.erb"
   end
 
   def new
@@ -26,7 +30,8 @@ class Client::ProductsController < ApplicationController
         input_name: params[:input_name],
         input_description: params[:input_description],
         input_price: params[:input_price],
-        input_stocked: params[:input_stocked]
+        input_stocked: params[:input_stocked],
+        input_supplier_id: params[:input_supplier_id]
       }
     response = Unirest.post("http://localhost:3000/api/products",
       parameters: client_params
@@ -34,6 +39,14 @@ class Client::ProductsController < ApplicationController
     flash[:success] = "You made a new product!"
     redirect_to "/client/products"
   end
+  
+  def show
+    response_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{response_id}")
+    @product = response.body
+    render "show.html.erb"
+  end
+
   def edit
     product_id = params[:id]
     response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
@@ -46,7 +59,8 @@ class Client::ProductsController < ApplicationController
       input_name: params[:input_name],
       input_description: params[:input_description],
       input_price: params[:input_price],
-      input_stocked: params[:input_stocked]
+      input_stocked: params[:input_stocked],
+      input_image_url: params[:input_image_url]
 
     }
     response = Unirest.patch("http://localhost:3000/api/products/#{params[:id]}",
@@ -57,8 +71,8 @@ class Client::ProductsController < ApplicationController
   end
 
   def destroy
-    flash[:danger] = "You deleted a recipe"
     response = Unirest.delete("http://localhost:3000/api/products/#{params[:id]}")
+    flash[:danger] = "You deleted a recipe"
     redirect_to "/client/products"
   end
 
