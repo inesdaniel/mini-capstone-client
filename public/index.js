4cf8ccb27ae07b6952bf7d4fce2fc467d204440c
Ines Daniel <i.danie8@gmail.com>
2018-08-02 20:38:16 -0500

    make index.html to replace .html.erb file, clone product cards

---
+++ public/index.js
@@ -0,0 +1,8 @@
+console.log('files linked');
+
+var productTemplate = document.querySelector('#product-card');
+var productContainer = document.querySelector('.row');
+
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+productContainer.appendChild(productTemplate.content.cloneNode(true));
+
